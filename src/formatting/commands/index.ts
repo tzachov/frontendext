01dd66183ed06ff22485fb7bbe523244dfd6222b
Tzach Ovadia <tzach@stuccomedia.com>
2017-06-17 11:31:36 +0300

    feat(create-barrel): add create barrel function
    closes ##7

--- src/formatting/commands/index.ts
+++ src/formatting/commands/index.ts
@@ -1,1 +1,2 @@
 export * from './format-json';
+export * from './create-barrel';
--- src/formatting/formatting.module.ts
+++ src/formatting/formatting.module.ts
@@ -1,10 +1,11 @@
 import * as vscode from 'vscode';
 
 import { ModuleWithCommands, CommandsContainer } from '../global';
-import { formatJson } from './commands';
+import { formatJson, createBarrel } from './commands';
 
 export class FormattingModule extends ModuleWithCommands implements CommandsContainer {
     registerCommands(context: vscode.ExtensionContext, namespace: string, options: { [key: string]: any; }) {
         this.addCommand(context, `${namespace}.formatJson`, formatJson);
+        this.addCommand(context, `${namespace}.createBarrel`, createBarrel);
     }
 }
